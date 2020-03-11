40949afb71127f412cdc686534e0163ed56bb1ee
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-03-11 13:52:48 +0000

    setting right data type

--- test_cupy_median_filter.py
+++ test_cupy_median_filter.py
@@ -27,7 +27,7 @@ filter_height = 3
 filter_width = 3
 filter_size = (filter_height, filter_width)
 
-cp_data = cp.random.uniform(low=0, high=10, size=(5, 5, 5))
+cp_data = cp.random.uniform(low=0, high=10, size=(8, 8, 8)).astype(DTYPE)
 np_data = cp_data.get()
 
 # Create a padded array in the GPU
@@ -49,9 +49,9 @@ cupy_median_filter(
 # Run the scipy median filter
 scipy_median_filter(np_data, size=filter_size)
 # Check that the results match
-print(np_data[0])
-print(cp_data.get()[0])
-assert np.allclose(np_data, cp_data.get())
+cp_result = cp_data.get()
+print(np.isclose(np_data, cp_result))
+assert np.allclose(np_data, cp_result)
 
 free_memory_pool([cp_data])
 
