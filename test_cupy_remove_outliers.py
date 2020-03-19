2559f5756f6511957a02572782808d0cf2ad4ac7
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-03-19 14:37:19 +0000

    accidentally sending entire padded array

--- test_cupy_remove_outliers.py
+++ test_cupy_remove_outliers.py
@@ -10,7 +10,7 @@ filter_height = size
 filter_width = size
 filter_size = (filter_height, filter_width)
 
-N = 5
+N = 4
 
 cp_data = cp.random.uniform(low=0, high=10, size=(N,N,N)).astype(DTYPE)
 np_data = cp_data.get()
@@ -21,18 +21,30 @@ pad_width = filter_size[0] // 2
 np_padded_data = create_padded_array(np_data, pad_width, pad_height, "reflect")
 cp_padded_data = cp.array(np_padded_data)
 
-diff = 1
+diff = 2.0
 
 cpu_result = tomopy.misc.corr.remove_outlier(np_data, diff, size)
 
 for i in range(N):
-    cupy_two_dim_remove_outliers(cp_data[i], cp_padded_data, diff, size, "light")
+    cupy_two_dim_remove_outliers(cp_data[i], cp_padded_data[i], diff, size, "light")
     gpu_result[i][:] = cp_data[i].get()
 
 cp.cuda.runtime.deviceSynchronize()
 
-print(cpu_result)
-print(gpu_result)
+print("Original data:")
+print(np_data[0])
+print("CPU:")
+print(cpu_result[0])
+print("GPU:")
+print(gpu_result[0])
 
+print("Is close: gpu result and cpu result")
 print(np.isclose(gpu_result[0], cpu_result[0]))
-assert np.allclose(gpu_result, cpu_result)
+print("Is close: gpu result and input data")
+print(np.isclose(gpu_result[0], np_data[0]))
+print("Is close: cpu result and input data")
+print(np.isclose(cpu_result[0], np_data[0]))
+
+for i in range(N):
+    assert np.allclose(gpu_result, cpu_result)
+
