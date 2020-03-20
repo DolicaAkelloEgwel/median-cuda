23b90e99bef4ed9d9ecfd038b46efcdd48fd22cc
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-03-20 14:18:46 +0000

    wrong argument for padded array

--- test_cupy_remove_outliers.py
+++ test_cupy_remove_outliers.py
@@ -6,14 +6,17 @@ from cupy_test_utils import create_padded_array, cupy_two_dim_remove_outliers
 from imagingtester import DTYPE
 
 size = 3
-N = 5
+N = 2
 
 cp_data = cp.random.uniform(low=0, high=20, size=(N, N, N)).astype(DTYPE)
 np_data = cp_data.get()
 gpu_result = np.empty_like(np_data)
 
-np_padded_data = create_padded_array(np_data, size, "reflect")
+np_padded_data = create_padded_array(np_data, size // 2, "reflect")
 cp_padded_data = cp.array(np_padded_data)
+print("Padded array:")
+print(cp_padded_data[0])
+print(cp_padded_data.shape)
 
 diff = 0.5
 
@@ -21,7 +24,7 @@ cpu_result = tomopy.misc.corr.remove_outlier(np_data, diff, size)
 
 for i in range(N):
     cp.cuda.runtime.deviceSynchronize()
-    cupy_two_dim_remove_outliers(cp_data[i], cp_padded_data[i], diff, size, "light")
+    cupy_two_dim_remove_outliers(cp_data[i], cp_padded_data[i], diff, size, "dark")
     cp.cuda.runtime.deviceSynchronize()
     gpu_result[i][:] = cp_data[i].get()
 
@@ -39,5 +42,4 @@ print(np.isclose(gpu_result[0], np_data[0]))
 print("Is close: cpu result and input data")
 print(np.isclose(cpu_result[0], np_data[0]))
 
-for i in range(N):
-    assert np.allclose(gpu_result[i], cpu_result[i])
+assert np.allclose(gpu_result, cpu_result)
